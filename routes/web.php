a6a2db278f7d5f6b37c9aecf1d342d01d15c1900
xu qian su <s3a932003@student.ncut.edu.tw>
2022-10-24 02:33:14 +0800

    練習5

--- routes/web.php
+++ routes/web.php
@@ -32,3 +32,11 @@ Route::get('hello/{name?}', function ($name = 'Everybody'){
 Route::get('aaa', function (){
     return 'aaa';
 });
+Route::get('dashboard', function (){
+    return 'dashboard';
+});
+Route::group(['prefix' => 'admin'], function(){
+    Route::get('dashboard', function (){
+        return 'admin dashboard';
+    });
+});
