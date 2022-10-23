92eb389686c94803622dfff63db054ce29cbeadf
xu qian su <s3a932003@student.ncut.edu.tw>
2022-10-24 02:14:20 +0800

    練習1-3

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,9 @@ Route::get('/', function () {
 Route::get('/', function () {
     return 'welcome';
 });
+Route::get('r2', function() {
+    return view('welcome');
+});
+Route::get('r1', function() {
+    return redirect('r2');
+});
