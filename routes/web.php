bddf815c6496f82b48359e7e87b09fff06c3d40d
xu qian su <s3a932003@student.ncut.edu.tw>
2022-10-24 02:59:06 +0800

    練習7

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -40,3 +41,4 @@ Route::group(['prefix' => 'admin'], function(){
         return 'admin dashboard';
     });
 });
+Route::get('home', [HomeController::class, 'index'])->name('home.index');
